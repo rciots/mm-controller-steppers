1767b5465c39c606773453f151f5b4e0ee5fdeb5
Mario Parra <mparram@gmail.com>
2025-04-28 14:08:52 +0200

    test new movement logic

--- code/app.js
+++ code/app.js
@@ -98,7 +98,7 @@ function startSerial(mksport) {
         });
         setTimeout(() => {
             console.log('Moving to initial position');
-            mksport.write('G0 X12 Y12 Z12.5\n', (err) => {
+            mksport.write('G0 X12 Y12.5 Z12\n', (err) => {
                 if (err) {
                     console.error('Error sending G-code:', err.message);
                 }
@@ -140,12 +140,12 @@ function startSerial(mksport) {
             console.log("phase: " + data);
             if (data === "preStart") {
                 console.log("Moving to initial position (preStart)");
-                mksport.write('G0 X12 Y12 Z12.5\n', (err) => {
+                mksport.write('G0 X12 Y12.5 Z12\n', (err) => {
                     if (err) console.error('Error sending G-code:', err.message);
                 });
             } else if (data === "start") {
                 console.log("Moving to initial position (start)");
-                mksport.write('G0 X12 Y12 Z12.5\n', (err) => {
+                mksport.write('G0 X12 Y12.5 Z12\n', (err) => {
                     if (err) console.error('Error sending G-code:', err.message);
                 });
             } else if (data === "end") {
@@ -251,8 +251,8 @@ function moveMotor(mksport, direction) {
 function calculateMotorHeights(direction, currentInclination) {
     const initialHeight = 13;
     const initialHeightA = 12;
-    const initialHeightB = 12;
-    const initialHeightC = 12.5;
+    const initialHeightB = 12.5;
+    const initialHeightC = 12;
 
     const motorPositions = [
         { x: -5, y: -8.66 },
